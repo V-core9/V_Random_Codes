915bb37d578489c03d5a32161577b43a19076b4d
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-29 00:11:57 +0200

    yeaaa RUNNINIG test

--- ps1.__Hyper-V_[generate_vm]_._win10/Hyper-V__CreateVM.ps1
+++ ps1.__Hyper-V_[generate_vm]_._win10/Hyper-V__CreateVM.ps1
@@ -3,7 +3,7 @@ New-VM -Name V_W.VM -MemoryStartupBytes 512MB -Path D:\V_WinTestEnv2021.local
 
 New-VHD -Path F:\V_WinTestEnv2021.V_W.VM_drive.vhdx -SizeBytes 60GB -Dynamic 
 Add-VMHardDiskDrive -VMName V_W.VM -Path F:\V_WinTestEnv2021.V_W.VM_drive.vhdx 
-Set-VMDvdDrive -VMName DC -CommandontrollerNumber 1 -Path 'C:\Users\v__V_\_V_WorkSpace\V_Projects._.[random_codes]\ps1.__Hyper-V_[generate_vm]_._win10\Win10_21H1_English_x64.iso'
+Set-VMDvdDrive -VMName DC -CommandontrollerNumber 1 -Path C:\Users\v__V_\_V_WorkSpace\V_Projects._.[random_codes]\ps1.__Hyper-V_[generate_vm]_._win10\Win10_21H1_English_x64.iso
 
 Start-VM –Name V_W.VM 
 
--- ps1.__Hyper-V_[generate_vm]_._win10/Hyper-V__CreateVM.sh
+++ ps1.__Hyper-V_[generate_vm]_._win10/Hyper-V__CreateVM.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-powershell -c "Start-Process powershell -ArgumentList 'powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File .\Hyper-V__CreateVM.ps1 ' -verb RunAs"
+powershell -c "Start-Process powershell -ArgumentList 'powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File Hyper-V__CreateVM.ps1 ' -verb RunAs"
 
