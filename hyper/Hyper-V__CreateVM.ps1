43145115d1576fff524bca15fc4a8b66b2428d8e
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-08 22:08:44 +0200

    yea

---
+++ hyper/Hyper-V__CreateVM.ps1
@@ -0,0 +1,15 @@
+
+New-VM -Name V_v_W.VM -MemoryStartupBytes 512MB -Path D:\V_V_v_WinTestEnv2021.local 
+
+New-VHD -Path F:\V_v_WinTestEnv2021.V_v_W.VM_drive.vhdx -SizeBytes 60GB -Dynamic 
+Add-VMHardDiskDrive -VMName V_v_W.VM -Path F:\V_v_WinTestEnv2021.V_v_W.VM_drive.vhdx 
+Set-VMDvdDrive -VMName DC -CommandontrollerNumber 1 -Path 'C:\Users\v__V_\_V_WorkSpace\V_Projects._.[random_codes]\ps1.__Hyper-V_[generate_vm]_._win10\Win10_21H1_English_x64.iso'
+
+Start-VM –Name V_v_W.VM 
+
+Get-VM V_v_W.VM 
+
+Stop-VM V_v_W.VM 
+
+# Configure dynamic memory  [ PS: stop it first ]
+Set-VMMemory -VMName DC -DynamicMemoryEnabled \$true -StartupBytes 512MB -MinimumByte 512 ";
--- ps1.__Hyper-V_[generate_vm]_._win10/Hyper-V__CreateVM.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-powershell -c "Start-Process powershell -ArgumentList 'powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File Hyper-V__CreateVM.ps1 ' -verb RunAs"
-
